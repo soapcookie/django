03f43ba8c729753ce1db8b4b59400b1394632f96
Sua <sgua0720@gmail.com>
2023-08-22 20:53:52 +0900

    커밋 메세지

--- third/forms.py
+++ third/forms.py
@@ -8,12 +8,13 @@ REVIEW_POINT_CHOICES = (
     ('2', 2),
     ('3', 3),
     ('4', 4),
-    ('5', 5),
+    ('5', 5)
 )
+
 
 class ReviewForm(ModelForm):
     class Meta:
-        model = Review  # Review 모델로 수정
+        model = Review
         fields = ['point', 'comment', 'restaurant']
         labels = {
             'point': _('평점'),
@@ -28,20 +29,40 @@ class ReviewForm(ModelForm):
             'point': forms.Select(choices=REVIEW_POINT_CHOICES)
         }
 
+
 class RestaurantForm(ModelForm):
     class Meta:
         model = Restaurant
-        fields = ['name', 'address']
+        fields = ['name', 'address', 'image', 'password']
         labels = {
             'name': _('이름'),
             'address': _('주소'),
+            'image': _('이미지 url'),
+            'password': _('게시물 비밀번호'),
         }
         help_texts = {
             'name': _('이름을 입력해주세요.'),
             'address': _('주소를 입력해주세요.'),
+            'image': _('이미지의 url을 입력해주세요'),
+            'password': _('게시물 비밀번호를 입력해주세요.'),
+        }
+        widgets = {
+            'password': forms.PasswordInput()
         }
         error_messages = {
             'name': {
-                'max_length': _("이름이 너무 깁니다. 30자 이하로 해주세요."),
+                'max_length': _('이름이 너무 깁니다. 30자 이하로 해주세요.')
             },
+            'image': {
+                'max_length': _('이미지 주소의 길이가 너무 깁니다. 500자 이하로 해주세요.')
+            },
+            'password': {
+                'max_length': _('비밀번호가 너무 깁니다. 20자 이하로 해주세요.')
+            }
         }
+
+
+class UpdateRestaurantForm(RestaurantForm):
+    class Meta:
+        model = Restaurant
+        exclude = ['password']
--- third/views.py
+++ third/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from third.models import Restaurant, Review
 from django.core.paginator import Paginator
-from third.forms import RestaurantForm, ReviewForm
+from third.forms import RestaurantForm, ReviewForm, UpdateRestaurantForm
 from django.http import HttpResponseRedirect
+from django.db.models import Count, Avg
 
+
+# Create your views here.
 def list(request):
-    restaurants = Restaurant.objects.all()
+    restaurants = Restaurant.objects.all().annotate(reviews_count=Count('review'))\
+        .annotate(average_point=Avg('review__point'))
     paginator = Paginator(restaurants, 5)
 
-    page = request.GET.get('page')
+    page = request.GET.get('page') ## third/list?page=1
     items = paginator.get_page(page)
 
     context = {
@@ -16,24 +20,27 @@ def list(request):
     }
     return render(request, 'third/list.html', context)
 
+
 def create(request):
     if request.method == 'POST':
         form = RestaurantForm(request.POST)
         if form.is_valid():
             new_item = form.save()
-            return HttpResponseRedirect('/third/list/')
-    else:
-        form = RestaurantForm()
+        return HttpResponseRedirect('/third/list/')
+    form = RestaurantForm()
     return render(request, 'third/create.html', {'form': form})
+
 
 def update(request):
     if request.method == 'POST' and 'id' in request.POST:
+        # item = Restaurant.objects.get(pk=request.POST.get('id'))
         item = get_object_or_404(Restaurant, pk=request.POST.get('id'))
-        form = RestaurantForm(request.POST, instance=item)
-        if form.is_valid():
+        password = request.POST.get('password', '')
+        form = UpdateRestaurantForm(request.POST, instance=item)
+        if form.is_valid() and password == item.password:
             item = form.save()
-            return HttpResponseRedirect('/third/list/')
     elif request.method == 'GET':
+        # item = Restaurant.objects.get(pk=request.GET.get('id'))  ##third/update?id=2
         item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
         form = RestaurantForm(instance=item)
         return render(request, 'third/update.html', {'form': form})
@@ -43,32 +50,48 @@ def update(request):
 def detail(request, id):
     if id is not None:
         item = get_object_or_404(Restaurant, pk=id)
-        reviews = Review.objects.filter(restaurant=item).all()  # Review.objects로 수정
+        reviews = Review.objects.filter(restaurant=item).all()
         return render(request, 'third/detail.html', {'item': item, 'reviews': reviews})
     return HttpResponseRedirect('/third/list/')
 
 
-def delete(request):
-    if 'id' in request.GET:
-        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
-        item.delete()
-    return HttpResponseRedirect('/third/list/')
+def delete(request, id):
+    item = get_object_or_404(Restaurant, pk=id)
+    if request.method == 'POST' and 'password' in request.POST:
+        if item.password == request.POST.get('password') or item.password is None:
+            item.delete()
+            return redirect('list')
+        return redirect('restaurant-detail', id=id)
+    return render(request, 'third/delete.html', {'item': item})
 
 
 def review_create(request, restaurant_id):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)  #
-        if form.is_valid():  # 데이터가 form 클래스에서 정의한 조건 (max_length 등)을 만족하는지 체크합니다.
-            new_item = form.save()  # save 메소드로 입력받은 데이터를 레코드로 추가합니다.
-        return redirect('restaurant-detail', id=restaurant_id)  # 전화면으로 이동합니다.
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            new_item = form.save()
+        return redirect('restaurant-detail', id=restaurant_id)
 
     item = get_object_or_404(Restaurant, pk=restaurant_id)
     form = ReviewForm(initial={'restaurant': item})
-    return render(request, 'third/review_create.html', {'form': form, 'item':item})
+    return render(request, 'third/review_create.html', {'form': form, 'item': item})
 
 
 def review_delete(request, restaurant_id, review_id):
     item = get_object_or_404(Review, pk=review_id)
     item.delete()
 
-    return redirect('restaurant-detail', id=restaurant_id)  # 전 화면으로 이동합니다.
+    return redirect('restaurant-detail', id=restaurant_id)
+
+
+def review_list(request):
+    reviews = Review.objects.all().select_related().order_by('-created_at')
+    paginator = Paginator(reviews, 10)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
+    context = {
+        'reviews': items
+    }
+    return render(request, 'third/review_list.html', context)
